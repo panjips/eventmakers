b1cd32850b1dbbcb2367c8c8b391e1efaefc001e
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-01-28 21:36:40 +0700

    feat: adding login hooks functionallity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
-import { Header } from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,10 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
