21bee4eb057f79aead15ef5f05ab3e249a12c9d3
Panji <panjipusaka71@gmail.com>
2024-02-11 19:06:05 +0800

    style: complete manage event UI

--- src/components/dashboard/components/dashboard.jsx
+++ src/components/dashboard/components/dashboard.jsx
@@ -7,7 +7,7 @@ export const Dashboard = () => {
     const { isLoading, dataEvent } = useRegisteredEvent();
     return (
         <div className="px-6 py-3 lg:py-6 w-full">
-            <h1 className="text-2xl lg:text-3xl text-slate-600 font-bold">
+            <h1 className="text-2xl lg:text-3xl font-bold">
                 Welcome back, Panji Pusaka
             </h1>
             <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 md:grid-cols-2 gap-4 mt-4 w-full">
