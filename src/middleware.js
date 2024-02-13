5cdee170df6446fd3781afbbeb0508464c39307b
Panji Pusaka <panjipusaka@Panjis-MacBook-Pro.local>
2024-02-13 17:05:59 +0800

    feat: add middleware dashboard

---
+++ src/middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import * as jose from 'jose';
+
+export async function middleware(request) {
+    const token = cookies().get('token')?.value;
+    const secretKey = new TextEncoder().encode(process.env.SECRET_KEY);
+
+    try {
+        console.log(secretKey);
+        console.log(token);
+        await jose.jwtVerify(token, secretKey);
+        return NextResponse.next();
+    } catch (error) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+}
+
+export const config = {
+    matcher: '/dashboard/:path*'
+};
