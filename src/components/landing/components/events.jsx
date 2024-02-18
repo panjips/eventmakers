9b8df4f7fa728fdbdd01b86177806fb57e3ba4fc
Panji Pusaka <panjipusaka@Panjis-MacBook-Pro.local>
2024-02-18 11:43:46 +0700

    feat: complete func landing

--- src/components/landing/components/events.jsx
+++ src/components/landing/components/events.jsx
@@ -2,23 +2,20 @@ import React from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 import Link from 'next/link';
 import { CardEvent } from './cardEvent';
+import useDataLanding from '../hooks/useDataLanding';
 
-export const Events = () => {
+export const Events = async () => {
+    const { handleDataEvents } = useDataLanding();
+    const datas = await handleDataEvents();
     return (
         <div>
             <h2 className="text-lg lg:text-2xl text-slate-900 font-semibold">
-                Current Events
+                All Events
             </h2>
             <div className="mt-2 lg:mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8 w-full">
-                <CardEvent />
-                <CardEvent />
-                <CardEvent />
-                <CardEvent />
-                <CardEvent />
-                <CardEvent />
-                <CardEvent />
-                <CardEvent />
-                <CardEvent />
+                {datas.map((e) => {
+                    return <CardEvent key={e.events.id} data={e.events} />;
+                })}
             </div>
         </div>
     );
